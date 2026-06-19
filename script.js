b6d5d1bec6bf578a83066b097b3405bf8ac68a11
chiranjeevi_vankam <chiranjeevivankam00@gmail.com>
2026-06-19 16:50:41 +0530

    index and css
    updating skills section styles and apearence

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
   document.getElementById("msg").innerText = "Thanks for your message! I'll get back to you soon.";
   this.reset();
 });
+// <!-- Skill bar animation -->
+const skills = document.querySelectorAll(".skill-card");
+
+skills.forEach(skill => {
+
+    const fill = skill.querySelector(".skill-fill");
+
+    skill.addEventListener("mouseenter", () => {
+        fill.style.width = fill.dataset.width + "%";
+    });
+
+    skill.addEventListener("mouseleave", () => {
+        fill.style.width = "0%";
+    });
+
+});
